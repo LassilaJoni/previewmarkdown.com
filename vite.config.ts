b2f9a30f42dac739608ba2a1f6e099d960048598
Joni Lassila <joni@lassila.live>
2025-03-13 21:57:35 +0200

    Updated vite config to include different pages.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'src/pages/editor/index.html',
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
