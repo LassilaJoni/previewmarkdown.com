d081936ea6829acb3ca51af25ea73a1cfaea8864
Joni Lassila <joni@lassila.live>
2025-03-13 22:07:29 +0200

    Update vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,19 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        login: 'src/pages/editor/index.html',
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'src/pages/editor/index.html'),
       },
     },
   },
