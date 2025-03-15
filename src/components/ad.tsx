6a4c4d64516f0d35c6b224a4305172795eb1bb18
Joni Lassila <joni@lassila.live>
2025-03-15 12:29:29 +0200

    Modify ad.tsx

--- src/components/ad.tsx
+++ src/components/ad.tsx
@@ -1,26 +1,38 @@
-import { JSX, useEffect, useRef } from 'react'
+import { JSX, useEffect, useRef } from 'react';
+
 export default function Banner(): JSX.Element {
-    const banner = useRef<HTMLDivElement>(null)
+  const banner = useRef<HTMLDivElement>(null);
 
-    const atOptions = {
-        key: 'd56f0c64f95aa0514ccf093c7b4854a4',
-        format: 'iframe',
-        height: 60,
-        width: 468,
-        params: {},
-    }
-    useEffect(() => {
+  const atOptions = {
+    key: 'd56f0c64f95aa0514ccf093c7b4854a4',
+    format: 'iframe',
+    height: 60,
+    width: 468,
+    params: {},
+  };
+
+  useEffect(() => {
     if (banner.current && !banner.current.firstChild) {
-        const conf = document.createElement('script')
-        const script = document.createElement('script')
-        script.type = 'text/javascript'
-        script.src = `//www.highperformancedformats.com/${atOptions.key}/invoke.js`
-        conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+      // Create a script to set the ad options
+      const conf = document.createElement('script');
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
 
-        banner.current.append(conf)
-        banner.current.append(script)
+      // Create the external ad script element
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = `https://www.highperformanceformat.com/${atOptions.key}/invoke.js`;
+      script.async = true;
+
+      // Append the scripts to the banner container
+      banner.current.append(conf);
+      banner.current.append(script);
     }
-}, [banner])
+  }, []);
 
-    return <div className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
+  return (
+    <div
+      className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center"
+      ref={banner}
+    ></div>
+  );
 }
